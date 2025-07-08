f35dffe539fa1a8f42d2929d38f7be962c7f4f07
Agille-cmd <EGEMATDENOV@GMAIL.COM>
2025-07-08 20:45:26 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
-#!/usr/bin/env python3
-from telegram.ext import Updater
+from bot.config import config  # Импортируем объект config
 from bot import tech_analyzer, user_data, user_messages
 from bot.core import setup_handlers
-from bot.config import config
+from telegram.ext import Updater
 import logging
 
 def main():
-    """Основная функция запуска бота"""
     try:
-        # Инициализация приложения
-        updater = Updater(config.TOKEN, use_context=True)
-        
-        # Настройка обработчиков
+        updater = Updater(config.TOKEN, use_context=True)  # Используем config.TOKEN
         setup_handlers(updater.dispatcher)
         
-        # Сохраняем ID бота
-        config.BOT_ID = updater.bot.id
-        
-        logging.info("Бот успешно запущен. Ожидание сообщений...")
+        logging.info("Бот запущен. Ожидание сообщений...")
         updater.start_polling()
         updater.idle()
-        
     except Exception as e:
-        logging.critical(f"Критическая ошибка: {str(e)}")
+        logging.critical(f"Ошибка: {str(e)}")
 
 if __name__ == '__main__':
     main()
